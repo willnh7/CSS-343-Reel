c22359dbf24b9efc22cc0ee14d9e7138ed619a57
willnh7 <sarmientowillsa@gmail.com>
2026-03-31 16:44:05 +0000

    Forgot to add the addition of the check for this folder, forgot to mention i am running a script that checks and executes the valgrind check and linter

--- Week1/checkit.sh
+++ Week1/checkit.sh
@@ -4,4 +4,4 @@ uname -r > $FILENAME
 date >> $FILENAME
 #NOTE:  The following command should all be on one line!
 clang-tidy *.cpp -checks="-*,bugprone-*,performance-*,clang-analyzer-*,readability-*,misc-*" -- -std=c++11 | tee -a $FILENAME
-#chmod +x checkit.sh
+#NOTE:chmod +x checkit.sh
