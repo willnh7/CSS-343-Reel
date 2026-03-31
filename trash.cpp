83cbea89d8aee5bd8f57b184960e4b11a5f77d73
willnh7 <sarmientowillsa@gmail.com>
2026-03-31 15:51:35 +0000

    On docker image now, development will be here

---
+++ trash.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "TEASJJJJAJJAKL" << endl;
+    cout << "Blah" << endl;
+}
+
+//clang-format -style=llvm -dump-config > .clang-format
