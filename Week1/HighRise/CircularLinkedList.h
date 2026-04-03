c5cd03299a5ef605d5475094fdb529191d285034
willnh7 <sarmientowillsa@gmail.com>
2026-04-03 03:42:40 +0000

    I have implemented the clear() fully, testing for leaks and bugs but need further testing. I have the logic done for the remove next as well as creating the skeletons for getCurrent() and finishing size() so that it accurately gives the size.

--- Week1/HighRise/CircularLinkedList.h
+++ Week1/HighRise/CircularLinkedList.h
@@ -1,8 +1,6 @@
 #ifndef CIRCULAR_LINKED_LIST_H
 #define CIRCULAR_LINKED_LIST_H
 
-
-
 // fill the list with the array items
 // default creates one node circular list 
 template<typename T> 
@@ -14,9 +12,10 @@ class CircularLinkedList  {
                 Node* next;
                 Node(T myData): data(myData), next(nullptr){}
         };
-        // Private vars for the circular list
-        Node* head;
-        Node* last;
+        // Private vars for the circular list ( initialized to null due to it being flagged by 
+        // valgrind)
+        Node* head = nullptr;
+        Node* last = nullptr;
     public:
         // Start with the constructors
         //TODO: create one node circular list, test
@@ -26,16 +25,28 @@ class CircularLinkedList  {
             head->next = head;
             last = head;
         }
-        // Circular (array given) 
-        //TODO: fill a list of appropriate size with array size
-        // Need one for each 
+        /**
+         * 
+         * 
+         * 
+         * 
+         * Precondition: called as CircularLinkedList<string> myList({"Ling", "Bob", "Harry"}); 
+         * The list should be created via a already formatted list
+         * Post condition: a newly created circular linked list.
+         * 
+         */
         CircularLinkedList(const std::initializer_list<T>& array){
-            std::cout << "This takes a ARRAY\n";
-            for(const T& val : array) {
-                std::cout << val <<"\n";
-                addToList(val);
+            // Empty list, nothing is done
+            if(array.size() == 0) {
+                std::cout << "Empty List\n";
+            } else {
+                 // if it is not a empty list it will create a array
+                std::cout << "This takes a ARRAY\n";
+                for(const T& val : array) {
+                    addToList(val);
+                }
             }
-           printList();
+           
         }
         /**
          * The destructor for our circular linked list that calls the function
@@ -43,11 +54,8 @@ class CircularLinkedList  {
          * 
          */
         ~CircularLinkedList() {
-            std::cout << "This is a destructor\n";
             clear();
-        }
-        
-            
+        } 
         /**
          * Helper to add to the list when we initialize using the initilizer 
          * list as a parameter
@@ -69,16 +77,68 @@ class CircularLinkedList  {
                 last = newNode;
             }
         }
+        /**
+         * This function deletes the next node from the head node,
+         * changing the pointers to reflect the deletion and also moving the head pointer to the node
+         * that was after the deleted one.
+         * Precondition: the int next is a constant
+         * Post condition: a shifted circular linked list, head begining from 
+         * the node after the deleted node
+         * EX: {"Ling", "Bob", "Jennet"} the head is Ling, after deletion, it should be
+         * Jennet.
+         */
+        std::string removeNext(int k) {
+            
+            std::string removed = "";
+            // the general logic is as follows
+            /** 
+             * First we check the size, if the size is only 1, return, so we need
+             * a size for the circular linked list(with tests). 
+             * 
+             * 
+             * so pseudo code would be, 
+             * check size > 1
+             * if not { return head}
+             * if  size > 1 {
+             *      Node* temp = head->next;
+             *      string removed = temp->data;
+             *      head->next = temp->next;
+             *      delete temp;
+             *      temp = nullptr;
+             *      
+             * //shifting the pointers
+             *      Node* tempHead = head->next;
+             *      last = head;
+             *      head = tempHead;
+             *      
+             * }
+             */
+            if(size() > 1) {
+                /**
+                 * Deletes the next node for the assignment which the next will be constant
+                 * k = 1
+                 */
+                Node* temp = head->next;
+                removed = temp->data;
+                head->next = temp->next;
+                // added to account for edge cases.
+                if(temp == last) {
+                    last = head;
+                }
+                delete temp;
+                temp = nullptr;
+                /*
+                Node* tempHead = head->next;
+                if(tempHead == last) {
 
-        void removeNext(int next) {
-            if(head != nullptr){return;}
-
-            // We don't want to remove when there is only one node, we will have a seperate clear function
-            if(head == last) {
-                return;
-            } else {
-
+                }
+                */
             }
+            return removed;
+        }
+
+        T getCurrent(){
+            return head->data;
         }
         /**
          * This function simply goes through our circular linked list and prints out all the 
@@ -91,7 +151,6 @@ class CircularLinkedList  {
             if(head == nullptr) {
                 return;
             }
-
             Node* curr = head;
             do{
                 std::cout << curr->data << " ";
@@ -103,19 +162,50 @@ class CircularLinkedList  {
          * and frees up memory.
          */
         void clear() {
+            // If the list is already empty, return
             if(head == nullptr) {
+                std::cout << "\nClearing a already empty list!\n";
                 return;
             }
+            // start with the head to start deletion
             Node* curr = head;
             Node* nextNode = curr->next;
+            // breaking the connection and essentially making it a regular
+            // linked list
+            last->next = nullptr;
+            // Since we start at the head, we must use do while 
+            while(curr != nullptr) {
+                // get the nextNode immediately since we know curr is not nullptr
+                // no errors.
+                nextNode = curr->next;
 
-            while(curr != head) {
+                std::cout << "\nDeleting Node: " << curr->data << "\n";
+                // If the next node havent reached the end, print which node we are moving to
+                // and assign the curr node to the next node we have to delete
+                if(nextNode != nullptr) {
+                    std::cout << "Moving to Node: " << nextNode->data << "\n";
+                }
+                // 
                 delete curr;
                 curr = nextNode;
-                nextNode = curr->next;
             }
-            
+            // This is to ensure there are no dangling ptrs
+            head = nullptr;
+            last = nullptr;
+                
         }
 
+        int size(){
+            int count = 0;
+            Node* curr = head;
+            if(curr == last) {
+                return count;
+            }
+            do {
+                count++;
+                curr = curr->next;
+            } while (curr != head);
+            return count;
+        }
 };
 #endif
--- Week1/HighRise/CircularLinkedListTests.cpp
+++ Week1/HighRise/CircularLinkedListTests.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main() {
     cout << "Bleht\n";
     CircularLinkedList<string> residents({"Laing", "Wilder",  "Frobisher"});
-    
+    CircularLinkedList<string> friends({});
+    residents.printList();
+    friends.printList();
+    // .clear() on a list before destructor,
+
+    cout << "\nThe size of the residents is: " << residents.size() << "\n";
+    cout << "\nThe size of the residents is: " << friends.size() << "\n";
     /*
-   
     while(residents.size() > 1) {
         string eaten = residents.removeNext(1);  // k == 1
         cout << "Eaten: " << eaten << "\n";
     }
-
-    string winner = residents.getCurrent();
-    cout << "The winner is " << winner << "!\n";
     */
+    const string winner = residents.getCurrent();
+    cout << "The winner is " << winner << "!\n";
 }
