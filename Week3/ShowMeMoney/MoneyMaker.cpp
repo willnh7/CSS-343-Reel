71f4fc5ea20032f555ac6f9a4e0cbef49507342a
willnh7 <sarmientowillsa@gmail.com>
2026-04-14 03:38:20 +0000

    started show me the money

---
+++ Week3/ShowMeMoney/MoneyMaker.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class SalesLead {
+    public:
+        string name;
+        int income;
+};
+
+class CustomComparator {
+    // Priority over income, then whoever has been in the queue first.
+    // so if this->income < that->income, return that?
+    // the 
+};
+/**
+ * This will test the functionality of the priority queue.
+ */
+void testMyMoney() {
+
+}
